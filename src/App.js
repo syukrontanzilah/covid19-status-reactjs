19721c4f1c840e9e2566887658e0b6fc2022f294
syukrontanzilah <syukrontanzilah@gmail.com>
2020-07-09 08:39:36 +0700

    get data api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+      stats: []
+    }
+  }
+
+  async componentDidMount() {
+    const resp = await fetch('https://api.covid19api.com/countries')
+    const countries = await resp.json()
+    this.setState({ countries })
+    this.state.countries.forEach(async country => {
+      const resp = await fetch(`https://api.covid19api.com/total/country/${country.Slug}`)
+      const data = await resp.json()
+      if (data.length)
+        this.setState(prevState => (
+          { stats: prevState.stats.concat(data[data.length - 1]) }
+        ))
+    })
+
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {
+          this.state.stats.map(country => <h1>{country.Country}</h1>)
+        }
+
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+        <App/>
+    </div>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
